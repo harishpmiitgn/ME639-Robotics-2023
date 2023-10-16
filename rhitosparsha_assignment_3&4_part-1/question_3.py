c6766b39e469f755c4d1bf41c1e418f923dc210e
rhitosparsha <141488762+rhitosparsha@users.noreply.github.com>
2023-10-16 06:47:16 +0530

    Uploaded files for Assignment 3&4 Part-1

--- rhitosparsha_assignment_3&4_part-1/question_3.py
+++ rhitosparsha_assignment_3&4_part-1/question_3.py
@@ -1,10 +1,6 @@
 import numpy as np
 
-def compute_jacobian_and_velocity(links, dh_parameters, joint_types=None):
-    if joint_types is None:
-        # Default assumption: All joints are revolute
-        joint_types = ["R"] * len(links)
-
+def compute_jacobian_and_velocity(links, dh_parameters, joint_types, end_effector_velocities):
     if len(links) != len(dh_parameters) or len(links) != len(joint_types):
         raise ValueError("Input lengths do not match")
 
@@ -16,9 +12,6 @@ def compute_jacobian_and_velocity(links, dh_parameters, joint_types=None):
 
     # Initialize the end-effector position
     end_effector_position = np.zeros(3)
-
-    # Initialize the end-effector velocity
-    end_effector_velocity = np.zeros(6)
 
     for i in range(len(links)):
         a, alpha, d, theta = dh_parameters[i]
@@ -42,7 +35,10 @@ def compute_jacobian_and_velocity(links, dh_parameters, joint_types=None):
         # Calculate the translational part of the Jacobian
         J[3:, i] = z_i_minus_1
 
-    return J, end_effector_position, end_effector_velocity
+    # Compute joint velocities
+    joint_velocities = np.linalg.pinv(J).dot(end_effector_velocities)
+
+    return J, end_effector_position, joint_velocities
 
 # Default values:
 links = [0.1, 0.2, 0.15]
@@ -52,18 +48,20 @@ dh_parameters = [
     [0.03, 0, 0, 0.15]
 ]
 joint_types = ["R", "R", "R"]
+end_effector_velocities = [0.1, 0.1, 0.1, 0, 0, 0]
 
 choice = int(input("Do you want to :\n1. Use default values?\n2. Enter your own values?\nEnter option no. : "))
 
 n = int(input("\nEnter the number of links : "))
 
-# Take inputs if user decides to input their own values
+# Take inputs if the user decides to input their own values
 if choice == 2:
-    print("\nEnter the following values row-wise, separated by spaces :")
-    links =  list(map(float, input("Enter link lengths in m  : ").split()))
-    e2 =  list(map(float, input("Enter values of DH Parameters (Format : a, alpha, d, theta) : ").split()))
+    print("\nEnter the following values row-wise, separated by commas :")
+    links =  list(map(float, input("Enter link lengths in m  : ").split(',')))
+    e2 =  list(map(float, input("Enter values of DH Parameters (Format : a, alpha, d, theta) : ").split(',')))
     dh_parameters = np.array(e2).reshape(n, 4)
-    joint_types =  list(map(str, input("Enter joint types (R or P) : ").split()))
+    joint_types =  list(map(str, input("Enter joint types (R or P) : ").split(',')))
+    end_effector_velocities = np.array(list(map(float, input("Enter end effector velocities (Format : v_x, v_y, v_z, w_x, w_y, w_z) :\n").split(','))))
 
 print("\nLink lengths (m) :")
 print(links)
@@ -71,12 +69,14 @@ print("\nDH Parameters (Format : a, alpha, d, theta) : ")
 print(dh_parameters)
 print("\nJoint types : ")
 print(joint_types)
+print("\nEnd-effector velocities : ")
+print(end_effector_velocities)
 
-jacobian, end_effector_position, end_effector_velocity = compute_jacobian_and_velocity(links, dh_parameters, joint_types)
+jacobian, end_effector_position, joint_velocities = compute_jacobian_and_velocity(links, dh_parameters, joint_types, end_effector_velocities)
 
 print("\nManipulator Jacobian:")
 print(jacobian)
 print("\nEnd-effector Position:")
 print(end_effector_position)
-print("\nEnd-effector Velocity:")
-print(end_effector_velocity)
+print("\nJoint Velocities:")
+print(joint_velocities)
