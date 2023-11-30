17317c65ea04dd48f000dcc3365dfedb016f0cdb
HrishiBirje <142831125+HrishiBirje@users.noreply.github.com>
2023-11-30 14:54:06 +0530

    Add files via upload

--- Assignment 5/Q5.py
+++ Assignment 5/Q5.py
@@ -1,27 +1,27 @@
 import numpy as np
 from math import atan2, sqrt
 
-def inverse_spherical_wrist(u):
+def inverse_spherical_wrist(U):
 
-    u13, u23, u31, u32, u33, u11, u21 = U[0, 2], U[1, 2], U[2, 0], U[2, 1], U[2, 2], U[0, 0], U[1, 0]
+    u12, u13, u22, u23, u31, u32, u33, u11, u21 = U[0,1], U[0, 2], U[1, 1], U[1, 2], U[2, 0], U[2, 1], U[2, 2], U[0, 0], U[1, 0]
 
     c1 = np.arctan2(u23, u33)
-    s1 = np.arctan2(-u13, np.sqrt(r23 ** 2 + r33 ** 2))
+    s1 = np.arctan2(-u13, np.sqrt(u23 ** 2 + u33 ** 2))
 
     c5 = np.cos(s1)
     if abs(c5) > 1e-6:
-        s5 = r33 / c5
-        theta5 = np.arctan2(s1 * r13 - c1 * r23, c1 * r13 + s1 * r23 - c5)
+        s5 = u33 / c5
+        theta5 = np.arctan2(s1 * u13 - c1 * u23, c1 * u13 + s1 * u23 - c5)
 
-        theta4 = np.arctan2(c1 * r23 - s1 * r13, -c1 * r33 + s1 * r23)
+        theta4 = np.arctan2(c1 * u23 - s1 * u13, -c1 * u33 + s1 * u23)
 
-        theta6 = np.arctan2(-s1 * r12 + c1 * r22, s1 * r11 - c1 * r21)
+        theta6 = np.arctan2(-s1 * u12 + c1 * u22, s1 * u11 - c1 * u21)
 
         return theta4, theta5, theta6
     else:
 
-        theta4 = np.arctan2(r21, r11)
-        theta6 = np.arctan2(r32, -r31)
+        theta4 = np.arctan2(u21, u11)
+        theta6 = np.arctan2(u32, -u31)
 
         return theta4, theta6
 
